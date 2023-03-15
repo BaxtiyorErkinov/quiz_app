c174116cf56142d48cd544efac09f12ccfc10dcc
BaxtiyorErkinov <baxa10th@gmail.com>
2023-03-15 12:53:08 +0500

    finall changes

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -6,28 +6,13 @@ const StyledTable = styled.table`
   caption-side: top;
   border: none;
   border-collapse: collapse;
-  /* border-collapse: separate; */
-  /* border-spacing: 5px 10px; */
 
   caption-side: bottom;
-  /* empty-cell: show | hide;  */
-  /* empty-cell is a property of table or the cells themselves */
 
-  /* vertical-align: baseline | sub | super | text-top | 
-                text-bottom | middle | top | bottom | 
-                <percentage> | <length> */
-
-  /* tbody {
-    vertical-align: top;
-  }              */
   td,
   th {
     border: none;
   }
-  /* td,
-  th {
-    border: 1px solid;
-  } */
 
   td {
     padding: 5px 10px;
@@ -67,12 +52,6 @@ const TableMarkup = <T extends { [key: string]: any }>({
 }: ITableProps<T>) => {
   return (
     <StyledTable>
-      {/* <caption>Culture about contries</caption>
-    <colgroup>
-      <col />
-      <col />
-      <col />
-    </colgroup> */}
       <thead>
         <tr>
           {titles.map((title, index) => (
@@ -89,13 +68,6 @@ const TableMarkup = <T extends { [key: string]: any }>({
           </tr>
         ))}
       </tbody>
-      {/* <tfoot>
-      <tr>
-        {titles.map((title, index) => (
-          <th key={index}>{title}</th>
-        ))}
-      </tr>
-    </tfoot> */}
     </StyledTable>
   );
 };
